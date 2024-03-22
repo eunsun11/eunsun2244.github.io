1f563d745667656ddb31a275a2230616840484ef
chae90 <pes1335@naver.com>
2024-03-22 12:36:16 +0900

    no message

--- portfolio/main/js/common.js
+++ portfolio/main/js/common.js
@@ -66,13 +66,6 @@ $(function(){
             if( bottom_of_window > bottom_of_object ){
                 $(this).addClass('ani');
             }                
-        });
-        $('.fade2').each(function(i){
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if( bottom_of_window > bottom_of_object ){
-                $(this).addClass('ani');
-            }                
-        });
+        });        
     });
 });
